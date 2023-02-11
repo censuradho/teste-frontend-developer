287c7b44eadb760b0408e95e245059195e9fe7bb
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-02-10 21:02:55 -0300

    dssad

--- web/src/context/auth/index.tsx
+++ web/src/context/auth/index.tsx
@@ -1,17 +1,20 @@
 import { paths } from "@/constants/routes";
 import { useLocalStorage } from "@/hooks";
+import { api } from "@/service/api";
 import { authService } from "@/service/api/auth";
 import { SignInWithEmailPasswordPayload } from "@/service/api/auth/types";
 import { useRouter } from "next/router";
-import { createContext, PropsWithChildren, useContext, useState } from "react";
+import { createContext, PropsWithChildren, useContext, useEffect, useState } from "react";
 import { Auth, AuthContextProps } from "./types";
 
 const AuthContext = createContext({} as AuthContextProps)
 
+export const AUTH_KEY = '@teste-frontend'
+
 export function AuthProvider ({ children }: PropsWithChildren) {
   const router = useRouter()
 
-  const [auth, setAuth] = useLocalStorage<Auth | null>('@teste-frontend', null)
+  const [auth, setAuth] = useLocalStorage<Auth | null>(AUTH_KEY, null)
   const [isLoading, setIsLoading] = useState(false)
 
   const handleLoginWithEmailPassword = async (payload: SignInWithEmailPasswordPayload) => {
@@ -30,6 +33,7 @@ export function AuthProvider ({ children }: PropsWithChildren) {
     router.push(paths.auth.signIn)
     setAuth(null)
   }
+
   
   return (
     <AuthContext.Provider
